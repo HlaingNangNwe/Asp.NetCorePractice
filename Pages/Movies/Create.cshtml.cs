23719767f0a91c2a4941d565ffb84bb12119512e
HlaingNangNwe <hnnnang2020@gmail.com>
2020-09-18 01:28:38 +0630

    first commit

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieRazor.Data;
+using MovieRazor.Models;
+
+namespace MovieRazor.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MovieRazor.Data.MovieRazorContext _context;
+
+        public CreateModel(MovieRazor.Data.MovieRazorContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
